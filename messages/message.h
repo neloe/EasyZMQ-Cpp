a8b46e429a027e6bf3741302481105bc8cda9ed7
Nathan Eloe <powerofazure@gmail.com>
2014-08-15 11:19:06 -0500

    documentation for the generic message class

--- messages/message.h
+++ messages/message.h
@@ -13,9 +13,31 @@ namespace zmqcpp
   {
     friend class BaseMessage<Message>;
     protected:
+      /*!
+       * \brief prepares the frames to be sent
+       * \pre None
+       * \post None
+       * \returns the prepared frames to send
+       */
       std::list <std::shared_ptr<std::string>> & prep_frames() {return m_frames;}
-      void unprep_frames() {}
+      /*!
+       * \brief cleans up after sending the frames
+       * \pre None
+       * \post None
+       */
+      void unprep_frames() {return }
+      /*!
+       * \brief Prepares to receive
+       * \pre None
+       * \post None
+       */
       void start_recv() {}
+      /*!
+       * \brief Determines whether or not more frames should be received
+       * \pre None
+       * \post None
+       * \returns false (only one frame should be received)
+       */
       bool recv_more() {return false;}
   };
 }
