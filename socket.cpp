68a1c5e3287f6f14839728041416c1ac07898f9b
Nathan Eloe <powerofazure@gmail.com>
2014-10-25 11:44:36 -0500

    add lgpl2.1 license header to all source files

--- socket.cpp
+++ socket.cpp
@@ -1,3 +1,20 @@
+/*
+  Copyright (c) Nathan Eloe, 2014
+  This file is part of EasyZMQ-Cpp.
+
+  EasyZMQ-Cpp is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  EasyZMQ-Cpp is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with EasyZMQ-Cpp.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /*!
  * \file socket.cpp
  * \author Nathan Eloe
